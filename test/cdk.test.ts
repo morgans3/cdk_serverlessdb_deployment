7571839b32a07f5e3b63b49f8f48d5c884a0dd91
Stewart Morgan <stewartmorgan86@gmail.com>
2021-04-18 09:10:47 +0100

    removed cdk-stack as no longer required

--- test/cdk.test.ts
+++ test/cdk.test.ts
@@ -1,6 +1,6 @@
 import { expect as expectCDK, haveResource } from "@aws-cdk/assert";
 import * as cdk from "@aws-cdk/core";
-import * as Cdk from "../lib/cdk-stack";
+// import * as Cdk from "../lib/cdk-stack";
 
 // test("SQS Queue Created", () => {
 //   const app = new cdk.App();
